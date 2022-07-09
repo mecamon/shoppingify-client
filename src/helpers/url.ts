9c09277b8f685f34d62fabebf035c7f98018a894
mecamon <arq.camejia@gmail.com>
2022-07-09 18:38:01 -0400

    SMALL CHANGE ON URL ENV

--- src/helpers/url.ts
+++ src/helpers/url.ts
@@ -1,5 +1,5 @@
 export function getBaseUrl(): string {
   // @ts-ignore
-  const baseURL = !process.env.PROD ? 'http://localhost:8080' : process.env.API_URL as string
+  const baseURL = !process.env.API_URL ? 'http://localhost:8080' : process.env.API_URL as string
   return baseURL
 }
